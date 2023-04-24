7904cbd03803a82079d68570513414961d4e40a9
Ed-CodePower <edwardcicioc@gmail.com>
2023-04-24 10:57:17 -0400

    add eventlister click for clear button

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ const result = document.getElementById("result_Text");
 const calculation = document.getElementById("screen_Text");
 
 function insert(num){
+    calculation.textContent += num;
     console.log(num);
 }
+
+document.getElementById("clear").addEventListener("click", function(){
+    calculation.textContent = "";
+    result.textContent = 0;
+});
