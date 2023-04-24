aab10fc81fe5f75bb3f92a0c60e989438662a620
Ed-CodePower <edwardcicioc@gmail.com>
2023-04-24 11:00:24 -0400

    Add event listener for back button

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ document.getElementById("clear").addEventListener("click", function(){
     calculation.textContent = "";
     result.textContent = 0;
 });
+
+document.getElementById("back").addEventListener("click", function(){
+    let exp = calculation.textContent;
+    calculation.textContent = exp.substring(0, exp.length -1);
+});
